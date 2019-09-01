96f1555e2e452169685fbace260241e731e8cd27
celciusjj <juanjjo99@hotmail.es>
2019-09-01 17:17:46 -0500

    hace eso pues hpta

--- frontend/src/components/order/orderUserItem.jsx
+++ frontend/src/components/order/orderUserItem.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 //import ModalOrderUserItem from "./modalOrderUserItem";
 class OrderUserItem extends React.Component {
   state = {
@@ -10,37 +13,23 @@ class OrderUserItem extends React.Component {
   render() {
     return (
       <div>
-        <li className="list-group-item">
-          <div className="row align-items-center text-center">
-            <div className="col-md-8 d-flex justify-content-between align-items-center ">
-              <p className="text-dark m-0">Id: {this.props.idItem}</p>
-              <p className="text-dark m-0">Precio total: {this.props.price}</p>
-            </div>
-            <br />
-            <div className=" d-flex ">
-              <button
-                type="button"
-                className="btn btn-primary mr-2"
-                onClick={this.onHandleOpenModal}
-              >
-                Ver orden
-              </button>
-              <button
-                type="button"
-                className="btn btn-danger"
-                onClick={this.props.deleteItem}
-              >
-                Eliminar
-              </button>
-              {/*
-                <ModalOrderUserItem
-                  showModal={this.state.openOrderModal}
-                ></ModalOrderUserItem>
-                */}
-            </div>
-          </div>
-        </li>
+        <Accordion defaultActiveKey="0">
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+              <div className="row align-items-center text-center">
+                <div className="col-md-8 d-flex justify-content-between align-items-center ">
+                  <p className="text-dark m-0">Id: {this.props.idItem}</p>
+                  <p className="text-dark m-0">Precio total: {this.props.price}</p>
+                </div>
+              </div>
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Hello! I'm the body</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
       </div>
+
     );
   }
 }
